2ea4a0053c9778818eade30a6e492e0c0054b1d1
n1cb0tt <nicbott@gmail.com>
2021-06-22 01:34:21 +0200

    change port

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/json-patch+json' }));
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
